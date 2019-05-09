72bd16d20f5ba706b9935ad1dd3c66e6894b0c27
Ricardo Lence <lencericardo@gmail.com>
2019-05-09 12:45:06 +0200

    creando la subscripcion de el componente movie-list

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent implements OnInit {
+export class MovieListComponent implements OnInit, OnDestroy {
+type:string;
+typeSubscription:any;
 
-  constructor() { }
+  constructor( private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit() {  
+    this.typeSubscription = this.route.params.subscribe( params => {
+      this.type = params.type
+    })
+  }
+
+  ngOnDestroy(){
+    this.typeSubscription.unsubscribe()
   }
 
 }
